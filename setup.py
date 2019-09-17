3609d6c264d035b092bda43d42986e229bc5bc0a
Pablo Romano <pablo.romano42@gmail.com>
2019-09-17 08:38:28 -0500

    Updated setup.py with name change

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 from distutils.util import convert_path
 
 main_ns = {}
-version_path = convert_path('statistics/_version.py')
+version_path = convert_path('stats/_version.py')
 with open(version_path, 'r') as version_file:
     exec(version_file.read(), main_ns)
 version = main_ns['__version__']
 
 setup(
-    name="statistics",
+    name="stats",
     author="Pablo Romano",
     description="Statistics for Python",
     version=version,
